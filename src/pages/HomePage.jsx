799a5a982b93d2579c8937a1cc794fcc02f55804
paras chamoli <paraschamoli2592002@gmail.com>
2026-01-18 20:38:53 +0530

    few more updates

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,19 +1,18 @@
-import Hero from '../components/Hero'
-import Services from '../components/Services'
-import Portfolio from '../components/Portfolio'
-import Testimonials from '../components/Testimonials'
-import Contact from '../components/Contact'
+import Hero from "../components/Hero";
+import Services from "../components/Services";
+import Testimonials from "../components/Testimonials";
+import Contact from "../components/Contact";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
       <Services />
-      <Portfolio />
+
       <Testimonials />
       <Contact />
     </>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
